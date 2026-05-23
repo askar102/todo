ea5837337b50cee483f5d297001f0b2ec752a0ff
askar102 <sergey.bolotny@icloud.com>
2026-05-23 08:04:45 +0500

    added: interaction with todoContainer

--- app/page.tsx
+++ app/page.tsx
@@ -10,16 +10,17 @@ import type { Task } from "@/types/task";
 export default function Home() {
     const [open, setOpen] = useState(false);
 
+    const [tasks, setTasks] = useState<Task[]>([]);
+
     function createNewTask(task: Task) {
-      console.log(task.title),
-      console.log(task.description)
+        setTasks((prevTasks) => [...prevTasks, task]);
     }
 
     return (
         <div className="flex flex-col flex-1 items-center bg-[rgb(224,224,224)]  font-sans">
             <main className="flex flex-1 w-full max-w-3xl flex-col items-center py-32 px-16  sm:items-start">
                 <Header onNewTaskButton={() => setOpen(true)}/>
-                <TodoContainer />
+                <TodoContainer tasks={tasks} />
 
                 {open && <TaskMaker onClose={() => setOpen(false)} onNewTask={createNewTask} />}
 
--- components/todoContainer.tsx
+++ components/todoContainer.tsx
@@ -1,7 +1,20 @@
-export default function TodoContainer() {
+import { useState } from "react";
+
+import type { Task } from "@/types/task";
+
+type TodoContainerProps = {
+    tasks: Task[];
+};
+
+export default function TodoContainer({ tasks }: TodoContainerProps) {
     return (
         <div className="w-full py-10 px-16 border border-gray-500 bg-[rgb(224,224,220)]">
-            
+            {tasks.map((task, index) => (
+                <div key={index}>
+                    <h2>{task.title}</h2>
+                    <p>{task.description}</p>
+                </div>
+            ))}
 
         </div>
     );
