d821abb89608a81f80bd8bca6ab1493549ad7d4c
askar102 <sergey.bolotny@icloud.com>
2026-05-23 18:27:00 +0500

    added: route.ts POST and GET method

---
+++ app/api/tasks/route.ts
@@ -0,0 +1,22 @@
+import { db } from "@/lib/db";
+
+export async function GET() {
+  const result = await db.query("SELECT * FROM tasks ORDER BY created_at DESC");
+
+  return Response.json(result.rows);
+}
+
+export async function POST(request: Request) {
+    const body = await request.json();
+  
+    const result = await db.query(
+      `
+      INSERT INTO tasks (title, description, status)
+      VALUES ($1, $2, $3)
+      RETURNING *
+      `,
+      [body.title, body.description, body.status ?? "active"]
+    );
+  
+    return Response.json(result.rows[0], { status: 201 });
+}
