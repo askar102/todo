c22493be7a7e69521623730e2e664504481dfb1a
askar102 <sergey.bolotny@icloud.com>
2026-05-23 08:24:09 +0500

    added: flex-col and gap-4 to TodoContainer

--- components/taskItem.tsx
+++ components/taskItem.tsx
@@ -8,7 +8,7 @@ type TaskItemProp = {
 
 export default function TaskItem({ task }: TaskItemProp) {
     return (
-        <div>
+        <div className="bg-red-600">
             <h2>{task.title}</h2>
             <p>{task.description}</p>
         </div>
